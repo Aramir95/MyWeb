2e5c761bc9f462fe1d0c6378aeb5b4b658dd1b07
angelramirez <angel.ramirez@unmsm.edu.pe>
2021-12-23 14:19:47 -0500

    spy-scroll

--- script.js
+++ script.js
@@ -16,3 +16,8 @@
     });
   }, false);
 })();
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
