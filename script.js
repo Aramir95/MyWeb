b2989c5031788d661772d21d70b5e77f823d2e7e
angelramirez <angel.ramirez@unmsm.edu.pe>
2021-12-31 11:33:14 -0500

    draft with efect matrix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Form Validation 
 (function() {
   'use strict';
   window.addEventListener('load', function() {
@@ -17,7 +17,42 @@
   }, false);
 })();
 
+//tooltip activation
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
+
+
+//matrix animation
+const canvas = document.getElementById('matrix');
+const ctx = canvas.getContext('2d');
+
+const w = canvas.width = document.body.offsetWidth;
+const h = canvas.height = document.body.offsetHeight;
+const cols = Math.floor(w / 20) + 1;
+const ypos = Array(cols).fill(0);
+
+ctx.fillStyle = '#000';
+ctx.fillRect(0, 0, w, h);
+
+function matrix () {
+  ctx.fillStyle = '#0001';
+  ctx.fillRect(0, 0, w, h);
+  
+  ctx.fillStyle = '#0f0';
+  ctx.font = '15pt monospace';
+  
+  ypos.forEach((y, ind) => {
+    const text = String.fromCharCode(Math.random() * 128);
+    const x = ind * 20;
+    ctx.fillText(text, x, y);
+    if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+    else ypos[ind] = y + 20;
+  });
+}
+
+setInterval(matrix, 50);
+
+
+
